fc56541c3392087cc02148490325c0148e333603
Szymon Wiśniewski <szymwisn@gmail.com>
2018-07-25 16:09:14 +0200

    Fixed functions.php

--- SeedMed - WP theme/functions.php
+++ SeedMed - WP theme/functions.php
@@ -1,9 +1,9 @@
 <?php
 
 function custom_theme_assets() {
-		wp_enqueue_style( 'style', get_stylesheet_uri() );
 		wp_enqueue_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css');
 		wp_enqueue_style( 'skeleton', get_template_directory_uri() . '/assets/css/skeleton.css');
+        wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
