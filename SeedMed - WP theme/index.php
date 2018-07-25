13b255edbff0f1b83a03e0443052b95abf284648
Tymek <tymoteusz@frankiewicz.com.pl>
2018-07-25 17:06:04 +0200

    Finished index.php

--- SeedMed - WP theme/index.php
+++ SeedMed - WP theme/index.php
@@ -1,1 +1,148 @@
 <?php /* Template Name: home */ ?>
+
+<?php
+
+get_header();
+
+?>
+
+<section class="hero-slider">
+    <div id="slider">
+        <div class="slides">
+            <!-- First slide -->
+            <div class="slider">
+                <div class="images">
+                    <img class="slider-photo" src="http://seedmed.pl/wp-content/uploads/2018/07/slider1_cropped.jpg" alt="surgeons on work">
+                </div>
+            </div>
+
+            <!-- Second slide -->
+            <div class="slider">
+                <div class="images">
+                    <img class="slider-photo" src="http://seedmed.pl/wp-content/uploads/2018/07/slider2_cropped.jpg" alt="examining roentgen's scan">
+                </div>
+            </div>
+
+            <!-- Third slide -->
+            <div class="slider">
+                <div class="images">
+                    <img class="slider-photo" src="http://seedmed.pl/wp-content/uploads/2018/07/slider3_cropped.jpg" alt="puppy at vet">
+                </div>
+            </div>
+
+            <!-- Fourth slide -->
+            <div class="slider">
+                <div class="images">
+                    <img class="slider-photo" src="http://seedmed.pl/wp-content/uploads/2018/07/slider4_cropped.jpg" alt="doctor">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="hero">
+        <div class="container">
+            <div class="flex">
+                <div class="hero-box first-box">
+                    <h3 class="reveal">Medycyna</h3>
+                    <p class="reveal-second">Znajdziesz tu produkty dla chirurgii, urologii, endoskopii, anestezjologi. jeżeli czegoś nie widzisz, zapytaj.</p>
+                    <a href="medicine.html" class="reveal-second"><button class="button-readmore">Czytaj</button></a>
+                </div>
+
+                <div class="hero-box second-box">
+                    <h3 class="reveal">Diagnostyka</h3>
+                    <p class="reveal-second">W tej zakładce prezentujemy asortyment do diagnostyki m.in. biopsja, terapia neoadjuwantowa czy też analizatory.</p>
+                    <a href="diagnosis.html" class="reveal-second"><button class="button-readmore">Czytaj</button></a>
+                </div>
+
+                <div class="hero-box third-box">
+                    <h3 class="reveal">Weterynaria</h3>
+                    <p class="reveal-second">Miejsce, w którym każdy weterynarz znajdzie dla siebie coś ciekawego, m. in. sprzęt do diagnostyki czy też monitorowania.</p>
+                    <a href="veterinary.html" class="reveal-second"><button class="button-readmore">Czytaj</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<!-- End Slider
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<!-- Offer
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<section class="offer">
+    <div class="container">
+        <div class="row">
+
+            <div class="twelve columns">
+                <h2>Czym się zajmujemy?</h2>
+            </div>
+
+        </div>
+        <div class="offer-boxes row">
+
+            <div class="one-third column">
+                <div class="offer-box">
+                    <div class="icon-circle reveal" style="-webkit-animation-duration: 1s;">
+                        <i class="icon-1 fas fa-comments fa-3x"></i>
+                    </div>
+                    <h3 class="reveal">Dobry plan na rozwój</h3>
+                    <p class="reveal-second">Pomożemy w rozwoju, wdrażaniu nowych technik oraz pokażemy nowoczesny sprzęt dający nowe możliwości.</p>
+                    <div style="text-align:center;"><button class="button-readmore reveal-third" style="-webkit-animation-duration: 1s;">Czytaj</button></div>
+                </div>
+            </div>
+
+            <div class="one-third column">
+                <div class="offer-box">
+                    <div class="icon-circle reveal" style="-webkit-animation-delay: 0.2s; -webkit-animation-duration: 1s;">
+                        <i class="icon-2 fas fa-check-circle fa-3x"></i>
+                    </div>
+                    <h3 class="reveal" style="-webkit-animation-delay: 0.2s;">Dobra firma w dobrym miejscu</h3>
+                    <p class="reveal-second" style="-webkit-animation-delay: 0.2s;">Lokacja naszej firmy gwarantuje możliwość szybkiego kontaktu. Powiedz nam jaki masz problem, a przyjedziemy by go rozwiązać.</p>
+                    <div style="text-align:center;"><button class="button-readmore reveal-third">Czytaj</button></div>
+                </div>
+            </div>
+
+            <div class="one-third column">
+                <div class="offer-box">
+                    <div class="icon-circle reveal" style="-webkit-animation-delay: 0.4s; -webkit-animation-duration: 1s;">
+                        <i class="icon-3 fas fa-users fa-3x"></i>
+                    </div>
+                    <h3 class="reveal" style="-webkit-animation-delay: 0.4s;">Specjaliści Zespół</h3>
+                    <p class="reveal-second" style="-webkit-animation-delay: 0.4s;">Pracujemy w grupie specjalistów, posiadających duże doświadczenie. Wspieramy, podpowiadamy, dzielimy się wiedzą.</p>
+                    <div style="text-align:center;"><button class="button-readmore reveal-third">Czytaj</button></div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- End Offer
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<!-- Newsletter
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<section class="newsletter">
+    <div class="tnp tnp-subscription">
+        <div class="container">
+            <form method="post" action="http://seedmed.pl/?na=s" onsubmit="return newsletter_check(this)">
+                <h4>Zapisz się na newsletter</h4>
+                <p>Jeśli potrzebujesz więcej informacji zapytaj. Chętnie Tobie pomożemy.</p>
+                <input type="hidden" name="nlang" value="">
+
+                <div style="text-align: center;" class="tnp-field tnp-field-email">
+                    <input class="tnp-email input-email reveal" type="email" name="ne" placeholder="e-mail" required>
+                </div>
+                <div class="tnp-field tnp-field-button"><input style="margin: 0 auto;" class="button-signup tnp-submit" type="submit" value="Subskrybuj">
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+<?php
+
+get_footer();
+
+?>
