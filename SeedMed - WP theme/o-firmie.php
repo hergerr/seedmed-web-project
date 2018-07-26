32c9e8cb4f8c9b8058c4ade64d86016d5aed8f11
Tymek <tymoteusz@frankiewicz.com.pl>
2018-07-26 08:39:52 +0200

    Updated o-firmie.php

--- SeedMed - WP theme/o-firmie.php
+++ SeedMed - WP theme/o-firmie.php
@@ -2,71 +2,83 @@
 
 
 <?php
- 
+
 get_header();
 
 ?>
 
 
-    <section class="company">
-        <div class="row">
-            <div class="six columns"><img class="reveal" src="<?php the_field('sekcja_#1-_obrazek'); ?>" alt="dentist's tools" /></div>
+<section class="company">
+    <div class="row">
+            <div class="six columns">
+                <img src="http://seedmed.pl/wp-content/uploads/2018/07/about-company1_cropped.jpg" alt="dentist's desk" class="reveal" />
+            </div>
+
             <div class="container">
                 <div class="six columns">
-                    <h3><?php the_field('sekcja_#1-_naglowek'); ?></h3>
-                    <?php the_field('sekcja_#1-_tekst'); ?>
-
-                </div>
+                <h3>O&nbsp;firmie</h3>
+                <p>
+                    Seedmed z siedzibą w Pobiedziskach działa na rynku od 2017&nbsp;r.
+                    <br><br> To polska firma medyczna, której celem jest wprowadzanie innowacyjnych rozwiązań w zakresie onkologii, endoskopii oraz innych dziedzin medycyny i weterynarii. Proponujemy często produkty i rozwiązania o unikatowym charakterze.
+                    <br><br> Dlatego też nawiązaliśmy współpracę z&nbsp;polskimi i&nbsp;zagranicznymi kontrahentami w&nbsp;celu pozyskania sprzętu o&nbsp;wysokich parametrach technicznych i użytkowych.<br><br>
+                </p>
             </div>
+            </div>
+
         </div>
-    </section>
-    <section class="strategy">
-        <div class="row">
-            <div class="six columns"><img class="reveal" src="<?php the_field('sekcja_#2-_obrazek'); ?>" alt="doctor's daily report" /></div>
-            <div class="container">
+</section>
+
+<section class="strategy">
+    <div class="row">
+        <div class="six columns">
+            <img src="http://seedmed.pl/wp-content/uploads/2018/07/about-company2_cropped.jpg" alt="doctor's daily report" class="reveal" />
+        </div>
+<div class="container">
+            <div class="six columns">
+            <h3>Strategia&nbsp;firmy</h3>
+            <ul>
+                <li>stała rozbudowa i&nbsp;zarządzanie portfelem innowacyjnych produktów w zakresie onkologii, chirurgii onkologicznej, diagnostyki onkologicznej, endoskopii oraz weterynarii.</li>
+                <li>stabilny rozwój firmy z&nbsp;działalności usługowej w oparciu o&nbsp;implementację innowacyjnych rozwiązań marketingowych,</li>
+                <li>świadczenie usług marketingowych, sprzedażowych oraz budowania świadomości marki dla sektora medycznego</li>
+            </ul>
+        </div>
+</div>
+    </div>
+</section>
+
+<section class="colors">
+    <div class="row">
+        <div class="six columns">
+            <img src="http://seedmed.pl/wp-content/uploads/2018/07/about-company3_cropped.jpg" alt="heart" class="reveal" />
+        </div>
+<div class="container">
                 <div class="six columns">
-                    <h3><?php the_field('sekcja_#2-_naglowek'); ?></h3>
-                    <ul>
-                        <li><?php the_field('sekcja_#2-_tekst_a'); ?></li>
-                        <li><?php the_field('sekcja_#2-_tekst_b'); ?></li>
-                        <li><?php the_field('sekcja_#2-_tekst_c'); ?></li>
-                        <li><?php the_field('sekcja_#2-_tekst_d'); ?></li>
-                    </ul>
-                </div>
-            </div>
+            <h3>Nasze kolory</h3>
+            <p>Kolorem naszej firmy jest szary i czerwony<br><br>
+                <span style="color:grey">SZARY</span> Oznacza opanowanie i&nbsp;pewność siebie, ale także niezależność i umiejętność krytycznego spojrzenia<br><br>
+                <span style="color:#cc1735">CZERWONY</span> Oznacza entuzjazm, agresywną etykę pracy&nbsp;i&nbsp;niezłomny, innowacyjny duch.</p>
         </div>
-    </section>
-    <section class="colors">
+</div>
+    </div>
+</section>
+
+<section class="services-summary">
+    <div class="container">
         <div class="row">
-            <div class="six columns"><img class="reveal" src="<?php the_field('sekcja_#3-_obrazek'); ?>" alt="heart" /></div>
-            <div class="container">
-                <div class="six columns">
-                    <h3><?php the_field('sekcja_#3-_naglowek'); ?></h3>
-                    <?php the_field('sekcja_#3-_tekst'); ?>
-
-                    <span style="color: grey;"><?php the_field('sekcja_#3-_kolor_a'); ?></span> <?php the_field('sekcja_#3-_kolor_a_opis'); ?>
-
-                    <span style="color: #cc1735;"><?php the_field('sekcja_#3-_kolor_b'); ?></span> <?php the_field('sekcja_#3-_kolor_b_opis'); ?>
-
-                </div>
+            <div class="twelve columns">
+                <h3>Korzystaj z usług najlepszych specjalistów</h3>
+                <p>W naszej pracy przestrzegamy najwyższych standardów etycznych.</p>
+                <p>Celem firmy jest wdrażanie rozwiązań i technologii przyczyniających się do wydłużenia i poprawy jakości życia pacjentów oraz wspieranie wszelkich aktywności edukacyjnych.</p>
+                <p>Powiedz nam czego potrzebujesz, a my znajdziemy to dla Ciebie.</p>
             </div>
         </div>
-    </section>
-    <section class="services-summary">
-        <div class="container">
-            <div class="row">
-                <div class="twelve columns">
-                    <h3><?php the_field('sekcja_#4-_naglowek'); ?></h3>
-                    <p><?php the_field('sekcja_#4-_tekst'); ?></p>
-                </div>
-            </div>
-        </div>
-    </section>
+    </div>
+</section>
 
 
 
 <?php
 
 get_footer();
- 
+
 ?>
