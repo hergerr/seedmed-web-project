95803ca8225ce762f8d3cec59cc55b41d667fdd2
Szymon Wiśniewski <szymwisn@gmail.com>
2018-07-26 22:25:32 +0200

    Minified CSS and JS

--- SeedMed - WP theme/functions.php
+++ SeedMed - WP theme/functions.php
@@ -1,8 +1,8 @@
 <?php
 
 function custom_theme_assets() {
-		wp_enqueue_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css');
-		wp_enqueue_style( 'skeleton', get_template_directory_uri() . '/assets/css/skeleton.css');
+		wp_enqueue_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.min.css');
+		wp_enqueue_style( 'skeleton', get_template_directory_uri() . '/assets/css/skeleton.min.css');
         wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 
@@ -16,31 +16,32 @@ add_action( 'wp_enqueue_scripts', 'seedmed_scripts' );
 
 function wpse_enqueue_page_template_styles() {
     if ( is_page_template( 'home.php' ) ) {
-        wp_enqueue_style( 'home', get_template_directory_uri() . '/assets/css/home.css' );
+        wp_enqueue_style( 'home', get_template_directory_uri() . '/assets/css/home.min.css' );
+        wp_enqueue_style( 'slider-def', get_template_directory_uri() . '/assets/css/slider-def.min.css' );
     }
 
     if ( is_page_template( 'o-firmie.php' ) ) {
-        wp_enqueue_style( 'o-firmie', get_template_directory_uri() . '/assets/css/o-firmie.css' );
+        wp_enqueue_style( 'o-firmie', get_template_directory_uri() . '/assets/css/o-firmie.min.css' );
     }
 
     if ( is_page_template( 'medycyna.php' ) ) {
-        wp_enqueue_style( 'medycyna', get_template_directory_uri() . '/assets/css/medycyna-diagnostyka-weterynaria.css' );
+        wp_enqueue_style( 'medycyna', get_template_directory_uri() . '/assets/css/medycyna-diagnostyka-weterynaria.min.css' );
     }
 
     if ( is_page_template( 'diagnostyka.php' ) ) {
-        wp_enqueue_style( 'diagnostyka', get_template_directory_uri() . '/assets/css/medycyna-diagnostyka-weterynaria.css' );
+        wp_enqueue_style( 'diagnostyka', get_template_directory_uri() . '/assets/css/medycyna-diagnostyka-weterynaria.min.css' );
     }
 
     if ( is_page_template( 'weterynaria.php' ) ) {
-        wp_enqueue_style( 'weterynaria', get_template_directory_uri() . '/assets/css/medycyna-diagnostyka-weterynaria.css' );
+        wp_enqueue_style( 'weterynaria', get_template_directory_uri() . '/assets/css/medycyna-diagnostyka-weterynaria.min.css' );
     }
 
     if ( is_page_template( 'wspolpraca.php' ) ) {
-        wp_enqueue_style( 'wspolpraca', get_template_directory_uri() . '/assets/css/wspolpraca.css' );
+        wp_enqueue_style( 'wspolpraca', get_template_directory_uri() . '/assets/css/wspolpraca.min.css' );
     }
 
     if ( is_page_template( 'kontakt.php' ) ) {
-        wp_enqueue_style( 'kontakt', get_template_directory_uri() . '/assets/css/kontakt.css' );
+        wp_enqueue_style( 'kontakt', get_template_directory_uri() . '/assets/css/kontakt.min.css' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'wpse_enqueue_page_template_styles' );
