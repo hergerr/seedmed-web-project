aaeae963c61f7ab6fe3f3acfaa01328841e26648
Tymek <tymoteusz@frankiewicz.com.pl>
2018-07-27 13:32:15 +0200

    Even more improved medycyna and o-firmie

--- SeedMed - WP theme/medycyna.php
+++ SeedMed - WP theme/medycyna.php
@@ -11,11 +11,11 @@ get_header();
 
 <section class="medicine-description">
         <div class="row">
-            <div class="five columns">
+            <div class="six columns">
                 <img src="http://seedmed.pl/wp-content/uploads/2018/07/slider1_cropped.jpg" alt="surgeons" width="400" class="reveal" />
             </div>
             <div class="container">
-            <div class="seven columns">
+            <div class="six columns">
                 <h3>The best care</h3>
                 <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you. ​ <br><br> This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company.
                 </p>
